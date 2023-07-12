0f06d53260a27d693d01e5b27e7716e61f0300d2
Aravindhan A <aravindhan.asaithambi@cdw.com>
2023-07-12 17:45:01 +0530

    Access specifiers demo

---
+++ Day-6/Library/Program.cs
@@ -0,0 +1,84 @@
+﻿
+using System;
+using System.Reflection;
+using Image;
+
+namespace Image
+{
+    class Editor
+    {
+        public string publicField = "Public";
+
+        private string privateField = "Private";
+
+        internal string internalField = "Internal";
+
+        protected string protectedField = "Protected";
+
+        private protected string privateProtectedField = "Private Protected";
+
+        protected internal string protectedInternalField = "Protected Internal";
+    }
+
+    class ImageEditor : Editor
+    {
+        public void subclassMethod()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Inside same assembly and a sub class");
+            Console.ResetColor();
+            Console.WriteLine("Can Access the following: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(this.publicField);
+            Console.WriteLine(this.internalField);
+            Console.WriteLine(this.protectedField);
+            Console.WriteLine(this.protectedInternalField);
+            Console.WriteLine(this.privateProtectedField);
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Cannot access: Private\n");
+            Console.ResetColor();
+        }
+    }
+
+    class UserInterface
+    {
+
+    }
+}
+
+public class Calling
+{
+
+    public static void listProperties(object obj)
+    {
+        PropertyInfo[] props = obj.GetType().GetProperties();
+        Console.WriteLine(props.Length);
+        foreach (var prop in props)
+        {
+            Console.WriteLine(prop.Name + ": " + prop.GetValue(obj, null));
+        }
+    }
+
+    public static void Main()
+    {
+        ImageEditor instance = new ImageEditor();
+
+        instance.subclassMethod();
+
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("Inside same assembly but not a sub class");
+        Console.ResetColor();
+        Console.WriteLine("Can Access the following: ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(instance.publicField);
+        Console.WriteLine(instance.internalField);
+        Console.WriteLine(instance.protectedInternalField);
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Cannot access: Private, Protected, PrivateProtected\n");
+        Console.ResetColor();
+    }
+}
---
+++ Day-6/OOPS/Program.cs
@@ -0,0 +1,49 @@
+﻿using Image;
+using System;
+
+class VideoEditor : Image.Editor
+{
+    public void anotherAssemblySubclass()
+    {
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("Different assembly and a sub class");
+        Console.ResetColor();
+        Console.WriteLine("Can Access the following: ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(this.publicField);
+        Console.WriteLine(this.protectedField);
+        Console.WriteLine(this.protectedInternalField);
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Cannot access: Private, Internal, Private Protected\n");
+        Console.ResetColor();
+
+    }
+}
+
+
+class OutsideClass
+{
+    static void Main()
+    {
+        Calling.Main();
+        VideoEditor currentClassInstance = new VideoEditor();
+        currentClassInstance.anotherAssemblySubclass();
+
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("Different assembly and not a sub class");
+        Console.ResetColor();
+        Console.WriteLine("Can Access the following: ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(currentClassInstance.publicField);
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Cannot access: Private, Internal, Private Protected, Protected, Protected Internal \n");
+        Console.ResetColor();
+
+        // Image.ImageEditor instance = new Image.ImageEditor();
+        // instance.subclassMethod();
+    }
+}
