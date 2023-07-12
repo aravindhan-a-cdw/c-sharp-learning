b5b92046574d24c865308dd92c76c348f0cecead
Aravindhan A <aravindhan.asaithambi@cdw.com>
2023-07-12 17:44:27 +0530

    Using ref and out instead of array

--- Day-3/AdditionMatrix/Program.cs
+++ Day-3/AdditionMatrix/Program.cs
@@ -3,24 +3,24 @@ using System;
 class AdditionMatix
 {
 
-    static int[] getAdditionCombo(int number)
+    static void getAdditionCombo(int number, out int number1, out int number2, ref int number3)
     {
         int[] output = new int[3];
         Random rand = new Random();
-        output[0] = rand.Next(1, number - 2);
-        output[1] = rand.Next(1, number - output[0]);
-        output[2] = number - output[0] - output[1];
+        number1 = rand.Next(1, number - 2);
+        number2 = rand.Next(1, number - number1);
+        number3 = number - number1 - number2;
 
-        // Console.WriteLine($"{output[0]} + {output[1]} + {output[2]} = {number}");
-        return output;
+        // Console.WriteLine($"{number1} + {number2} + {number3} = {number}");
     }
     static void Main()
     {
         Console.Write("Enter a number: ");
         int input = Convert.ToInt32(Console.ReadLine());
-        int[] combo = getAdditionCombo(input);
-        Console.WriteLine($"{combo[0]} {combo[1]} {combo[2]}");
-        Console.WriteLine($"{combo[1]} {combo[2]} {combo[0]}");
-        Console.WriteLine($"{combo[2]} {combo[0]} {combo[1]}");
+        int number1, number2, number3 = 0;
+        getAdditionCombo(input, out number1, out number2, ref number3);
+        Console.WriteLine($"{number1} {number2} {number3}");
+        Console.WriteLine($"{number2} {number3} {number1}");
+        Console.WriteLine($"{number3} {number1} {number2}");
     }
 }
