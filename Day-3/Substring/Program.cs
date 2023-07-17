2b0de9bc8207be1f3f34c846256ae44a258efe0b
Aravindhan A <aravindhan.asaithambi@cdw.com>
2023-07-17 17:57:36 +0530

    Removed using inbuilt function

--- Day-3/Substring/Program.cs
+++ Day-3/Substring/Program.cs
@@ -8,16 +8,23 @@ class Substring
         string inputString = args[0];
         int trimLength = Convert.ToInt32(args[1]);
 
-        if (trimLength > inputString.Length)
+        if (trimLength * 2 > inputString.Length)
         {
             Console.WriteLine("The string length must be greater than the number length!");
             Environment.Exit(1);
         }
 
         StringBuilder output = new StringBuilder();
-
-        output.Append(inputString.Substring(0, trimLength));
-        output.Append(inputString.Substring(inputString.Length - trimLength));
+        int index = 0;
+        while (index < inputString.Length)
+        {
+            if (index == trimLength)
+            {
+                index = inputString.Length - trimLength;
+            }
+            output.Append(inputString[index]);
+            ++index;
+        }
 
         Console.WriteLine($"Output: {output}");
     }
